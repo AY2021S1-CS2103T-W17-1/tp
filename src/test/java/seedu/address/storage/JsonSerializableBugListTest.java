d634a3e1e36dba801a876c55fc0e6c89bbf187a2
the-reefshark <rishabh92k@gmail.com>
2020-09-24 20:58:02 +0800

    Update tests

--- src/test/java/seedu/address/storage/JsonSerializableBugListTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableBugListTest.java
@@ -22,8 +22,8 @@ public class JsonSerializableBugListTest {
 
     @Test
     public void toModelType_typicalPersonsFile_success() throws Exception {
-        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(TYPICAL_PERSONS_FILE,
-                JsonSerializableAddressBook.class).get();
+        JsonSerializableBugList dataFromFile = JsonUtil.readJsonFile(TYPICAL_PERSONS_FILE,
+                JsonSerializableBugList.class).get();
         BugList bugListFromFile = dataFromFile.toModelType();
         BugList typicalPersonsBugList = TypicalPersons.getTypicalAddressBook();
         assertEquals(bugListFromFile, typicalPersonsBugList);
@@ -31,16 +31,16 @@ public class JsonSerializableBugListTest {
 
     @Test
     public void toModelType_invalidPersonFile_throwsIllegalValueException() throws Exception {
-        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(INVALID_PERSON_FILE,
-                JsonSerializableAddressBook.class).get();
+        JsonSerializableBugList dataFromFile = JsonUtil.readJsonFile(INVALID_PERSON_FILE,
+                JsonSerializableBugList.class).get();
         assertThrows(IllegalValueException.class, dataFromFile::toModelType);
     }
 
     @Test
     public void toModelType_duplicatePersons_throwsIllegalValueException() throws Exception {
-        JsonSerializableAddressBook dataFromFile = JsonUtil.readJsonFile(DUPLICATE_PERSON_FILE,
-                JsonSerializableAddressBook.class).get();
-        assertThrows(IllegalValueException.class, JsonSerializableAddressBook.MESSAGE_DUPLICATE_PERSON,
+        JsonSerializableBugList dataFromFile = JsonUtil.readJsonFile(DUPLICATE_PERSON_FILE,
+                JsonSerializableBugList.class).get();
+        assertThrows(IllegalValueException.class, JsonSerializableBugList.MESSAGE_DUPLICATE_PERSON,
                 dataFromFile::toModelType);
     }
 
